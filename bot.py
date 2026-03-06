179153c47e12c77c463584d9aae76fb5e6b4701f
yaroslesk-sketch <yaroslesk@icloud.com>
2026-03-06 05:48:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,58 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.filters import Command
 
-TOKEN = "8570820876:AAF0S8AaDACmwxWBnnQ0eZBRZc9nxJjaM7g"
+TOKEN = "8570820876:AAEaz_o-1JkkdTwDbaDvlqTYAGQKPiSEUL4"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+# Клавиатура с кнопками
+keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Контакты", callback_data="contacts")],
+    [InlineKeyboardButton(text="Доставка", callback_data="delivery")]
+])
+
+
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer(
-        "Добро пожаловать.\n\n"
-        "Доступные команды:\n"
-        "/command1 — Контакты\n"
-        "/command2 — Доставка"
+        "Добро пожаловать.\nВыберите раздел:",
+        reply_markup=keyboard
     )
 
 
 @dp.message(Command("command1"))
 async def contacts(message: Message):
     await message.answer(
-        "Контакты:\n"
-        "Телефон: 89025445443"
+        "Контакты:\nТелефон: 89025445443"
     )
 
 
 @dp.message(Command("command2"))
 async def delivery(message: Message):
     await message.answer(
-        "Доставка:\n"
-        "Свяжитесь с нами по номеру 89025445443"
+        "Доставка:\nСвяжитесь с нами по номеру 89025445443"
     )
+
+
+# обработка нажатия кнопок
+@dp.callback_query()
+async def buttons(callback: CallbackQuery):
+
+    if callback.data == "contacts":
+        await callback.message.answer(
+            "Контакты:\nТелефон: 89025445443"
+        )
+
+    elif callback.data == "delivery":
+        await callback.message.answer(
+            "Доставка:\nСвяжитесь с нами по номеру 89025445443"
+        )
+
+    await callback.answer()
 
 
 async def main():
